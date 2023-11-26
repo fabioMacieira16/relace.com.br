2b800990feb00b60f7ae550db5d24fd9510a9387
Fabio Macieira <fabiomacieira16@gmail.com>
2023-11-25 22:26:55 -0300

    Refactor form.js and index.html

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,40 +1,44 @@
 class FormSubmit {
-    constructor(settings) {
-      this.settings = settings;
-      this.form = document.querySelector(settings.form);
-      this.formButton = document.querySelector(settings.button);
-      if (this.form) {
-        this.url = this.form.getAttribute("action");
+  constructor(settings) {
+    this.settings = settings;
+    this.form = document.querySelector(settings.form);
+    this.formButton = document.querySelector(settings.button);
+    if (this.form) {
+      this.url = this.form.getAttribute("action");
+    }
+    this.sendForm = this.sendForm.bind(this);
+  }
+
+  displaySuccess() {
+    this.form.innerHTML = this.settings.success;
+  }
+
+  displayError() {
+    this.form.innerHTML = this.settings.error;
+  }
+
+  getFormObject() {
+    const formObject = {};
+    const fields = this.form.querySelectorAll("[name]");
+    fields.forEach((field) => {
+      formObject[field.getAttribute("name")] = field.value;
+    });
+    return formObject;
+  }
+
+  onSubmission(event) {
+    event.preventDefault();
+    event.target.disabled = true;
+    event.target.innerText = "Enviando...";
+  }
+
+  async sendForm(event) {
+
+    try {
+      if (this.validateAndSubmit()) {
+        alert('Por favor, preencha todos os campos antes de enviar o formulário.');
       }
-      this.sendForm = this.sendForm.bind(this);
-    }
-  
-    displaySuccess() {
-      this.form.innerHTML = this.settings.success;
-    }
-  
-    displayError() {
-      this.form.innerHTML = this.settings.error;
-    }
-  
-    getFormObject() {
-      const formObject = {};
-      const fields = this.form.querySelectorAll("[name]");
-      fields.forEach((field) => {
-        formObject[field.getAttribute("name")] = field.value;
-      });
-      console.log("#objeto",formObject);
-      return formObject;
-    }
-  
-    onSubmission(event) {
-      event.preventDefault();
-      event.target.disabled = true;
-      event.target.innerText = "Enviando...";
-    }
-  
-    async sendForm(event) {
-      try {
+      else {
         this.onSubmission(event);
         await fetch(this.url, {
           method: "POST",
@@ -45,26 +49,73 @@ class FormSubmit {
           body: JSON.stringify(this.getFormObject()),
         });
         this.displaySuccess();
-      } catch (error) {
-        this.displayError();
-        throw new Error(error);
       }
-    }
-  
-    init() {
-      if (this.form) this.formButton.addEventListener("click", this.sendForm);
-      return this;
+    } catch (error) {
+      this.displayError();
+      throw new Error(error);
     }
   }
-  
-  const formSubmit = new FormSubmit({
-    form: "[data-form]",
-    button: "[data-button]",
-    success: "<h1 class='success'>Mensagem enviada!</h1>",
-    error: "<h1 class='error'>Não foi possível enviar sua mensagem.</h1>",
-  });
-  formSubmit.init();
 
+  init() {
+    if (this.form) this.formButton.addEventListener("click", this.sendForm);
+    return this;
+  }
+
+  //#region -- validação de campos
+  validateAndSubmit() {
+    // Obtenha os valores dos campos
+    const name = this.form.elements['name'].value;
+    const email = this.form.elements['email'].value;
+    const phone = this.form.elements['phone'].value;
+    const cargo = this.form.elements['$cargo'].value;
+    const empresa = this.form.elements['$empresa'].value;
+    const calcular = this.form.elements['calcular'].value;
+
+    if (this.validaCalculoSubmit(calcular)) {
+      alert('Por favor, preencha o campo de cálculo corretamente');
+      return true;
+    }
+
+    if (!this.validaTermosSubmit()) {
+      alert('Por favor, aceite os termos e condições antes de enviar o formulário.');
+      return true;
+    }
+
+    // Realize a validação (por exemplo, verifique se os campos não estão vazios)
+    if (name.trim() === '' || email.trim() === '' ||
+        phone.trim() === '' || cargo.trim() === '' ||
+        empresa.trim() === '' || calcular.trim() === '') {
+       return true;
+    }
+  }
+
+  validaCalculoSubmit(calcular) {
+    if (calcular.trim() !== '5') {
+      return true;
+    }
+  }
+
+  validaTermosSubmit() {
+    const checkbox = document.getElementById('aceitoTermos');
+
+    if (checkbox.checked) {
+      return true;
+    }
+  }
+  ////#endregion
+}
+
+
+
+const formSubmit = new FormSubmit({
+  form: "[data-form]",
+  button: "[data-button]",
+  success: "<h1 class='success'>Mensagem enviada!</h1>",
+  error: "<h1 class='error'>Não foi possível enviar sua mensagem.</h1>",
+});
+formSubmit.init();
+
+// ==================== functio usado para whatsapp ====================
 // Função para verificar se a expressão é válida
 // function isValidExpression(expression) {
 //     try {
